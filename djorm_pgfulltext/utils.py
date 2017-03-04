1ad8bddf06201e3c89b1e4a483f819297710ec6a
Atul Varma <varmaa@gmail.com>
2017-03-03 19:19:09 -0500

    Ensure connection exists when adapt() is called.

--- djorm_pgfulltext/utils.py
+++ djorm_pgfulltext/utils.py
@@ -5,6 +5,7 @@ from django.utils.text import force_text
 
 
 def adapt(text):
+    connection.ensure_connection()
     a = psycopg2.extensions.adapt(force_text(text))
     a.prepare(connection.connection)
     return a
