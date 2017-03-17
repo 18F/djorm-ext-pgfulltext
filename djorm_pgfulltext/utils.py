179c45929288e6b14f3067489c1ba8f9e158a974
Atul Varma <varmaa@gmail.com>
2017-03-17 13:48:46 -0400

    Unwrap connection if needed.

--- djorm_pgfulltext/utils.py
+++ djorm_pgfulltext/utils.py
@@ -7,5 +7,11 @@ from django.utils.text import force_text
 def adapt(text):
     connection.ensure_connection()
     a = psycopg2.extensions.adapt(force_text(text))
-    a.prepare(connection.connection)
+    c = connection.connection
+
+    # This is a workaround for https://github.com/18F/calc/issues/1498.
+    if hasattr(c, '__wrapped__'):
+        c = getattr(c, '__wrapped__')
+
+    a.prepare(c)
     return a
